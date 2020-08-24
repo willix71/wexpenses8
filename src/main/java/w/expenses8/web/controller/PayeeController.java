6040542185d16b02b817195c6dff0225cf278464
willi <willi@DESKTOP-Q8PDCHV>
2020-08-24 16:26:48 +0200

    first version of a payee controller

---
+++ src/main/java/w/expenses8/web/controller/PayeeController.java
@@ -0,0 +1,46 @@
+package w.expenses8.web.controller;
+
+import java.util.List;
+
+import javax.faces.view.ViewScoped;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import w.expenses8.data.domain.criteria.PayeeCriteria;
+import w.expenses8.data.domain.model.Payee;
+import w.expenses8.data.domain.service.IPayeeService;
+
+@Slf4j
+@Named
+@ViewScoped
+@Getter @Setter
+public class PayeeController {
+
+	@Inject
+	private IPayeeService payeeService;
+	
+	private PayeeCriteria criteria = new PayeeCriteria();
+	private List<Payee> payees;
+	private Payee selectedPayee;
+
+	public List<Payee> getPayees() {
+		if (payees == null) {
+			refresh();
+		}
+		return payees;
+	}
+	
+	public void refresh() {
+		log.debug("Resetting payees matching {}", criteria);
+		payees = payeeService.findPayees(criteria);
+	}
+	
+	public void reset() {
+		log.debug("Resetting payee criteria");
+		criteria.setText(null);
+		refresh();
+	}
+}
