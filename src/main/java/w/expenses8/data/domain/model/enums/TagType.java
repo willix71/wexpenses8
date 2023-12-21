1d8131dcc9336f1ad60d5f8d508e5df79fa97db7
willix <willix71@gmail.com>
2023-12-21 15:04:55 +0100

    Added WARNING tag type

--- src/main/java/w/expenses8/data/domain/model/enums/TagType.java
+++ src/main/java/w/expenses8/data/domain/model/enums/TagType.java
@@ -4,7 +4,7 @@ import w.expenses8.data.domain.criteria.TagCriteria;
 
 public enum TagType implements TagCriteria {
 
-	SYSTEM, ASSET, LIABILITY, INCOME, EXPENSE, DISCRIMINATOR, CONSOLIDATION, FLAG, GROUPING;
+	SYSTEM, ASSET, LIABILITY, INCOME, EXPENSE, DISCRIMINATOR, CONSOLIDATION, FLAG, WARNING, GROUPING;
 
 	@Override
 	public String getName() {
